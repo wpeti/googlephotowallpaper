87457b66cfd8ba70a54b8ff75a07319d8781e946
wpeti <winkler.peter1@gmail.com>
2020-02-13 22:34:15 +0100

    Update MyGooglePhotosRESTClientService.cs
    Removed commented code parts

--- GoogleApiTest/GooglePhotoWallpaperREST/MyGooglePhotosRESTClientService.cs
+++ GoogleApiTest/GooglePhotoWallpaperREST/MyGooglePhotosRESTClientService.cs
@@ -10,15 +10,7 @@ namespace GooglePhotoWallpaperREST
     {
         public MyGooglePhotosRESTClientService(Initializer initializer) : base(initializer)
         {
-            //Console.WriteLine("Apikey: ", ApiKey);
-            //Console.WriteLine("ApplicationName: ", ApplicationName);
-            //Console.WriteLine("HttpClient base addr: ", base.HttpClient.BaseAddress.ToString());
         }
-
-        //public async Task<string> HttpGet(string requestUri)
-        //{
-        //    return await HttpClient.GetStringAsync("");
-        //}
         public override string Name => throw new NotImplementedException();
 
         public override string BaseUri => throw new NotImplementedException();
